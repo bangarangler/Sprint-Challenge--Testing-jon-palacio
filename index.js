d2946c1d3ac768e500edae97b5521134edc1eabe
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-12 11:19:01 -0400

    Boiler plate for index, server, dbConfig
    also added testing info to knexfile and hooked it up in the dbConfig

---
+++ index.js
@@ -0,0 +1,7 @@
+const server = require('./api/server.js')
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+console.log(`running on localhost:${port}, ... testing is gooooood(subjective)`)
+})
