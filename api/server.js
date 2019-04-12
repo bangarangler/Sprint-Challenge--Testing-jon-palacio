c5e505f2b71927cf337620cc2beb89bb54a98afc
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-04-12 13:20:34 -0400

    Stretch :  Added endpoints and test for dup
    duplicate title test wrote up. endpoint to delete by id and endpoint to
    get by id

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Games = require("../games/gamesModel.js");
+const db = require("../data/dbConfig.js");
 
 const server = express();
 
@@ -20,15 +21,14 @@ server.get("/games", async (req, res) => {
   }
 });
 
+// SOME STRETCH REQUIRMENTS IN HERE WITH NORMAL WORK
 server.post("/games", async (req, res) => {
   const { title, genre, releaseYear } = req.body;
   if (!title || !genre || !releaseYear) {
-    res
-      .status(422)
-      .json({
-        message: `You must provide title, genre, and release year!`,
-        error: true
-      });
+    res.status(422).json({
+      message: `You must provide title, genre, and release year!`,
+      error: true
+    });
   } else {
     try {
       const newGame = await Games.insert(req.body);
@@ -40,13 +40,39 @@ server.post("/games", async (req, res) => {
         res.status(404).json({ message: "Error creating game in database" });
       }
     } catch (err) {
-      res
-        .status(405)
-        .json({
-          message: `Error processing your request, ${err}`,
-          error: true
-        });
+      res.status(405).json({
+        message: `Not Allowed, ${err}`,
+        error: true
+      });
     }
+  }
+});
+
+// STRETCH GET ENDPOINT FOR SINGLE GAME
+server.get("/games/:id", async (req, res) => {
+  const gameId = req.params.id;
+  try {
+    const GAME = await db("games")
+      .where({ id: gameId })
+      .first();
+    res.status(200).json(GAME);
+  } catch (err) {
+    res.status(500).json({ message: `Internal error, ${err}` });
+  }
+});
+
+// STRETCH DELETE ENDPOINT FOR A SINGLE GAME
+server.delete("/games/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const tempGame = await db("games")
+      .where({ id })
+      .del();
+    return tempGame
+      ? res.status(200).json(tempGame)
+      : res.status(404).json({ message: `Can't find game for that id` });
+  } catch (err) {
+    res.status(500).json({ message: `Internal Error, ${err}`, error: true });
   }
 });
 
--- api/server.spec.js
+++ api/server.spec.js
@@ -7,6 +7,7 @@ describe("server.js", () => {
     await db("games").truncate();
   });
 
+  //  SHOULD RETURN { api: "up" } FROM GET / ENDPOINT
   it('should return { api: "up" }', async () => {
     const res = await request(server).get("/");
     expect(res.body).toEqual({ api: "up" });
@@ -17,10 +18,12 @@ describe("server.js", () => {
       const res = await request(server).get("/games");
       expect(res.status).toBe(200);
     });
+    // SHOULD RETURN JSON FOR GET /GAMES ENDPOINT
     it("should return JSON", async () => {
       const res = await request(server).get("/games");
       expect(res.type).toBe("application/json");
     });
+    // TEST TO RETURN EMPTY ARRAY IF NO GAMES IN DB
     it("should return array if empty", async () => {
       const response = await request(server).get("/games");
       expect(function(res) {
@@ -34,6 +37,7 @@ describe("server.js", () => {
       const res = await request(server).post("/games");
       expect(res.status).toBe(422);
     });
+    // TEST SHOULD RETURN 200 STATUS CODE
     it("should return status 200 OK", async () => {
       const newGame = { title: "Test", genre: "works", releaseYear: 2019 };
       const res = await request(server)
@@ -41,9 +45,23 @@ describe("server.js", () => {
         .send(newGame);
       expect(res.status).toBe(200);
     });
+    // TEST TO RETURN JSON FOR POST /GAME ENDPOINT
     it("should return JSON", async () => {
       const res = await request(server).get("/games");
       expect(res.type).toBe("application/json");
+    });
+
+    // STRETCH THROW ERROR IF DUPICATE TITLE IS ATTEMPTED TEST
+    it("should throw err if duplicate title is attempted", async () => {
+      const newAttempt = { title: "Jon", genre: "stretch", releaseYear: 2019 };
+      const newAttempt2 = { title: "Jon", genre: "Me", releaseYear: 2017 };
+      const result = await request(server)
+        .post("/games")
+        .send(newAttempt);
+      const result2 = await request(server)
+        .post("/games")
+        .send(newAttempt2);
+      expect(result2.status).toBe(405);
     });
   });
 });
